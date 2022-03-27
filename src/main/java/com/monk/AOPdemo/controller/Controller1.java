84452e63f583cc32c4499b57fa1445b8c9ad4e05
chemmonk <l312940@gmail.com>
2022-03-27 21:17:24 +0800

    [feat] getmapping method add requestParams annotation

--- src/main/java/com/monk/AOPdemo/controller/Controller1.java
+++ src/main/java/com/monk/AOPdemo/controller/Controller1.java
@@ -1,6 +1,7 @@
 package com.monk.AOPdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,11 @@ public class Controller1 {
 	
 	
 	@GetMapping("/1")
-	public String firstGetMethod() {
+	public String firstGetMethod(@RequestParam("ak") String ak) {
+		
+		System.out.println(ak);
+		System.out.println(GetMethod());
+		
 		return "hello1";
 	}
 }
