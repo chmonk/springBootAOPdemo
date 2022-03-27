18d1da9e903c746e2e13e7f73377664a1e82a4c5
chemmonk <l312940@gmail.com>
2022-03-27 21:16:18 +0800

    [feat] add lombok dependency
    1. add slf4j write log
    2. print request info

--- src/main/java/com/monk/AOPdemo/aspect/LoggingAspect.java
+++ src/main/java/com/monk/AOPdemo/aspect/LoggingAspect.java
@@ -1,29 +1,60 @@
 package com.monk.AOPdemo.aspect;
 
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
+	
 	@Before("execution(public String GetMethod())")
 	public void LoggingAdviceForMethod() {
-		System.out.println("aspect to method signature");
+		
+		System.out.println("aspect: @Before(\"execution(public String GetMethod())\")");
 	}
-	
+
 	@AfterReturning("execution(public String GetMethod())")
 	public void returning() {
-		System.out.println("after return 1");
+		System.out.println("aspect: @AfterReturning(\"execution(public String GetMethod())\")");
 	}
-	
 
 	@Before("execution(public * com.monk.AOPdemo.controller.*.*(..)))")
-	public void LoggingAdviceForClass() {
-		System.out.println("aspect to class signature");
+	public void LoggingAdviceForClass(JoinPoint joinPoint) {
+		System.out.println("aspect: @Before(\"execution(public * com.monk.AOPdemo.controller.*.*(..)))\")");
+		
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        // 記錄下請求內容
+        log.info("=== Request Start ===");
+        log.info("請求 Url : " + request.getRequestURL().toString());
+        log.info("請求 Method : " + request.getMethod());
+        log.info("請求 IP : " + request.getRemoteAddr());
+        log.info("請求 Class : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("請求 參數 : " + Arrays.toString(joinPoint.getArgs()));
+		
 	}
 	
-
+	@AfterReturning("execution(public * com.monk.AOPdemo.controller.*.*(..)))")
+	public void returning2() {
+		System.out.println("aspect: @AfterReturning(\"execution(public * com.monk.AOPdemo.controller.*.*(..)))\")");
+	}
+	
+	
+	@Pointcut("execution(public * com.monk.AOPdemo.controller.*.*(..)))")
+	public void alldelegate() {
+	}
 
 }
